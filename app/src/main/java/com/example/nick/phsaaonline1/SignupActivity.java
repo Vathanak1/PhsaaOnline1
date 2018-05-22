52de868204c9fcc36b3f9be8de33de26f68e4857
Vathanak1 <ticnick92@gmail.com>
2018-05-22 14:09:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nick/phsaaonline1/SignupActivity.java
@@ -0,0 +1,18 @@
+package com.example.nick.phsaaonline1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nick on 5/17/2018.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().setTitle("Signup");
+    }
+}
